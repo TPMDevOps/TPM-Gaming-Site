b220767006a3c078e0b1e58801d0983e3a53c0eb
VictorLNegron <NegronVictor882@gmail.com>
2022-08-26 15:18:29 -0400

    Added backEnd

---
+++ react-app/backend/Gaming-site-BackEnd/src/main/java/TPM/DevOps/Gamingsite/BackEnd/GamingSiteBackEndApplication.java
@@ -0,0 +1,13 @@
+package TPM.DevOps.Gamingsite.BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingSiteBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamingSiteBackEndApplication.class, args);
+	}
+
+}
---
+++ react-app/backend/Gaming-site-BackEnd/src/test/java/TPM/DevOps/Gamingsite/BackEnd/GamingSiteBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package TPM.DevOps.Gamingsite.BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamingSiteBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
