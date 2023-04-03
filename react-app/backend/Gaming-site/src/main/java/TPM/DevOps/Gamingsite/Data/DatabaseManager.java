579355032dd322df5b092f9e41ccdedb371766c9
VictorLNegron <negronvictor882@gmail.com>
2023-04-03 15:40:31 -0400

    updates

---
+++ react-app/backend/Gaming-site/src/main/java/TPM/DevOps/Gamingsite/Data/DatabaseManager.java
@@ -0,0 +1,21 @@
+package TPM.DevOps.Gamingsite;
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+
+import javax.sql.DataSource;
+
+
+public class DatabaseManager {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/mydatabase";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Arcangel123!@#";
+
+    public static DataSource getDataSource() {
+        MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setURL(URL);
+        dataSource.setUser(USER);
+        dataSource.setPassword(PASSWORD);
+        return dataSource;
+    }
+}
