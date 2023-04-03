26a602a6d38260c9b38ddf898de1431a734e8cb8
VictorLNegron <102873788+VictorLNegron@users.noreply.github.com>
2023-04-03 15:50:52 -0400

    Update LoginController.java

--- react-app/backend/Gaming-site/src/main/java/TPM/DevOps/Gamingsite/Controller/LoginController.java
+++ react-app/backend/Gaming-site/src/main/java/TPM/DevOps/Gamingsite/Controller/LoginController.java
@@ -1,2 +1,18 @@
-package TPM.DevOps.Gamingsite.Controller;public class LoginController {
+package TPM.DevOps.Gamingsite.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/logout")
+    public String logout() {
+        return "logout";
+    }
 }
